09bf2f3e4e60d1bb24d667b01ccee2c1e482c908
Shebnem Bayezit <sebnembayezit2022@gmail.com>
2023-04-21 22:08:04 -0400

    Java Examples

--- src/test/java/arrays/Q10_AnElementExistInArrays.java
+++ src/test/java/arrays/Q10_AnElementExistInArrays.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class Q10_AnElementExistInArrays {
 
     /*
@@ -22,7 +24,33 @@ public class Q10_AnElementExistInArrays {
                 System.out.println("String s does not exist in arrays");
             }
 
+        System.out.println();
+        System.out.println("******* for integer arrays exist or not*********");
+        int a[] = {32, 14, 2, 11};
+        int b=14;
 
+        int m=0;
+        for(int w:a){
+            if(w==b){
+                m++;
+
+            }
+        }
+
+        if(m>0){
+            System.out.println("Exist");
+        }else{
+            System.out.println("not exist");
+        }
+
+        System.out.println();
+        System.out.println("******* for integer arrays exist or not  2.way*********");
+
+        int a2[] = {32, 14, 2, 11};
+        int b2=14;
+
+        Arrays.sort(a2);//[2,11,14,32]
+        System.out.println(Arrays.binarySearch(a2, 14));
 
     }
 }
